d6f39e6874b89459479c5d618ebc1dc10da96f08
sinmem <15196995314@163.com>
2018-05-29 00:00:44 +0800

    .

--- Task5/xt.py
+++ Task5/xt.py
@@ -1,14 +1,37 @@
 import sys
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (QApplication, QBoxLayout, QDialog,
-        QDialogButtonBox, QGridLayout, QLabel, QLineEdit, QTextEdit,
-        QVBoxLayout, QWidget)
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import (QApplication, QBoxLayout, QDialog, QComboBox,
+                             QDialogButtonBox, QGridLayout, QLabel, QLineEdit,
+                             QTextEdit, QVBoxLayout, QWidget)
 
-app = QApplication(sys.argv)
-w = QWidget()
-w.resize(250, 150)
-w.move(300, 300)
-w.setWindowTitle('简单窗口')
-w.show()
 
-sys.exit(app.exec_())
+class Example(QWidget):
+
+    def _init_(self):
+        super().__init__()
+        self.initUI()
+    
+    def initUI(self):
+
+        combo = QComboBox(self)
+        combo.addItem('Easy')
+        combo.addItem('Normal')
+        combo.addItem('Hard')
+
+        combo.move(50, 50)
+
+        combo.activated[str].connect(self.onActivated)
+
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('QComboBox')
+        self.show()
+
+    def onActivated(self, text):
+        print(text)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    # ex.show()
+    sys.exit(app.exec_())
